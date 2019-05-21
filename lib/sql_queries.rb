a9be394a50eaf08bb786f3f8fb6107475e0f53fd
Mary Lark <mary@marylark.com>
2019-05-21 23:07:52 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -14,7 +14,10 @@ ORDER BY Projects.title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"Write your SQL query Here"
+"SELECT Users.name, Users.age, Pledges.amount
+FROM Users
+INNER JOIN Pledges ON Pledges.user_id = Users.id
+ORDER BY Users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
