2ae3b3dba8636ee261c06ea1ad1d61b4e327f3d3
Mary Lark <mary@marylark.com>
2019-05-21 23:03:32 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,10 +7,10 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"SELECT Projects.names, Pledges.amounts 
+"SELECT Projects.title, Pledges.amounts 
 FROM Projects
-INNER JOIN Pledges ON Pledges.project_id = Projects.project_id
-ORDER BY Projects.names
+INNER JOIN Pledges ON Pledges.project_id = Projects.id
+ORDER BY Projects.title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
