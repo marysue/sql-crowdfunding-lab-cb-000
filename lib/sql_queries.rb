3aa49558aaa571e14910eb9736adf9d4c82e1cf9
Mary Lark <mary@marylark.com>
2019-05-21 22:51:20 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -9,7 +9,8 @@
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
 "SELECT Projects.names, Pledges.amounts 
 FROM Projects
-INNER JOIN Pledges ON Pledges.
+INNER JOIN Pledges ON Pledges.project_id = Projects.project_id
+ORDER BY Projects.names
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
