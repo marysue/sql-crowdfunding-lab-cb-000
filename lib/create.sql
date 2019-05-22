ab314aba31250d0e71399666338319f3f30eedc2
marysue <mary@marylark.com>
2019-05-22 11:28:20 -0600

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,21 @@
+CREATE TABLE Projects (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	title TEXT,
+	category TEXT,
+	funding_goal INTEGER,
+	start_date TEXT,
+	end_date TEXT
+);
+
+CREATE TABLE Pledges (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	amount INTEGER,
+	user_id INTEGER,
+	project_id INTEGER
+);
+
+CREATE TABLE Users (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	name TEXT,
+	age INTEGER
+);
--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,25 +7,54 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"Write your SQL query Here"
+"SELECT Projects.Title, SUM(Pledges.amount) as pledge_amount
+FROM Projects
+INNER JOIN Pledges ON Pledges.project_id = Projects.id
+GROUP BY Projects.Title
+ORDER BY Projects.Title;
+"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"Write your SQL query Here"
+  #The problem here was because we were using two values in the select statement,
+  #we needed to group by both of the values in the GROUP BY statement.
+  #If we attempted to GROUP BY jjust one of those columns we would error out.
+ "SELECT users.name, users.age, SUM(pledges.amount)
+ FROM users JOIN pledges ON users.id = pledges.user_id
+ GROUP BY users.name, users.age;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+"SELECT Projects.title, SUM(Pledges.amount) - Projects.funding_goal AS 'overfunded'
+FROM Projects
+INNER JOIN Pledges ON Projects.id = Pledges.project_id
+GROUP BY Projects.title, Projects.funding_goal
+HAVING
+    SUM(Pledges.amount) >= Projects.funding_goal;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"Write your SQL query Here"
+#When I group by pledges_amount, it lists all of the individual pledges, so I get
+#two of the same users within the list.  Ordering by creates unique user names, and
+#returns the actual sum of all pledges with the same users name.
+"SELECT Users.name, SUM(Pledges.amount) as pledges_amount
+FROM Users
+INNER JOIN Pledges ON Pledges.user_id = Users.id
+GROUP BY Users.name
+ORDER BY pledges_amount,  Users.name;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+"SELECT Projects.category, Pledges.amount
+FROM Pledges
+INNER JOIN Projects ON Pledges.project_id = Projects.id
+WHERE Projects.category LIKE 'music';"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"Write your SQL query Here"
+"SELECT Projects.category, SUM(Pledges.amount) as amount
+FROM Projects
+INNER JOIN Pledges ON Projects.id = Pledges.project_id
+WHERE Projects.category LIKE 'books'
+GROUP BY Projects.category;"
 end
